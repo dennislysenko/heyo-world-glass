cb688d7677d0c4a6e51df1db32bc1805037abb17
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2014-10-04 04:48:24 -0400

    tweaks

--- app/src/main/java/com/denniscourt/myapplication/LiveCardMenuActivity.java
+++ app/src/main/java/com/denniscourt/myapplication/LiveCardMenuActivity.java
@@ -2,19 +2,47 @@ package com.denniscourt.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+
+import java.util.Locale;
 
 /**
  * A transparent {@link Activity} displaying a "Stop" options menu to remove the {@link com.google.android.glass.timeline.LiveCard}.
  */
-public class LiveCardMenuActivity extends Activity {
+public class LiveCardMenuActivity extends Activity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+    private boolean initialized = false;
+    private String queuedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tts = new TextToSpeech(this /* context */, this /* listener */);
+    }
 
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         // Open the options menu right away.
         openOptionsMenu();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            initialized = true;
+            tts.setLanguage(Locale.ENGLISH);
+
+            if (queuedText != null) {
+                speak(queuedText);
+            }
+        }
     }
 
     @Override
@@ -28,7 +56,8 @@ public class LiveCardMenuActivity extends Activity {
         switch (item.getItemId()) {
             case R.id.action_stop:
                 // Stop the service which will unpublish the live card.
-                stopService(new Intent(this, HeyoCardService.class));
+                //stopService(new Intent(this, HeyoCardService.class));
+                speak("Heyo stop Hayo stop Hey o stop Hey oh stop Hay oh");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -40,5 +69,25 @@ public class LiveCardMenuActivity extends Activity {
         super.onOptionsMenuClosed(menu);
         // Nothing else to do, finish the Activity.
         finish();
+    }
+
+    public void speak(String text) {
+        // If not yet initialized, queue up the text.
+        if (!initialized) {
+            queuedText = text;
+            return;
+        }
+        queuedText = null;
+        // Before speaking the current text, stop any ongoing speech.
+        tts.stop();
+        // Speak the text.
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        // On any motion event (including touchpad tap), say 'Hello Glass'
+        speak("Hello Glass");
+        return true;
     }
 }
